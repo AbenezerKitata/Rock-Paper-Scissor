f8f96903121dd73716e250e84e346a6d581708f9
AbenezerKitata <92657524+AbenezerKitata@users.noreply.github.com>
2021-12-20 00:52:53 -0800

    counter created

--- app.js
+++ app.js
@@ -4,46 +4,29 @@ return rpsGameArray[~~(Math.random() * 3)].toLowerCase();
 }
 let buttons = document.querySelectorAll('.buttons')
 let result = document.getElementById('result');
+let counter;
   function playRound (button, computer) {
     let newDiv = document.createElement('p')
     if (button === computer)
-    {    newDiv.textContent = "Its a draw";
+    {   newDiv.textContent = "Its a draw";
+    counter = 0;
+
     newDiv.classList.add('draw');
     result.appendChild(newDiv)}  
     else if (button === "scissor" && computer === "rock" || button === "paper" && computer === "scissor" || button === "rock" && computer === "paper")
     {
     newDiv.textContent = `You entered ${button}, and the computer entered ${computer} you lost`;
+    newDiv.classList.add('lose');
     result.appendChild(newDiv)} 
     else  {
     newDiv.textContent = `You entered ${button}, and the computer entered ${computer} you won!`;
+    newDiv.classList.add('win');
     result.appendChild(newDiv)} 
   }
 buttons.forEach(btn=>{
   btn.addEventListener('click', ()=>{
     let img = btn.querySelector('img')
     let playerSelection = img.id;
-  let computerResult = computerPlay();
+    let computerResult = computerPlay();
   playRound(playerSelection, computerResult)
-  }, false)})
-
-
-
- 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+  })})
