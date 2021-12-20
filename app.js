ccc62728eb7a01ca57d7bc2853bbde1d4a65e2fe
AbenezerKitata <92657524+AbenezerKitata@users.noreply.github.com>
2021-12-19 23:58:03 -0800

    made it work with pictures as buttons

--- app.js
+++ app.js
@@ -1,77 +1,36 @@
-let r = document.getElementById('rock');
-let p = document.getElementById('paper');
-let s = document.getElementById('scissor');
-let gameArray = document.querySelectorAll('buttons')
-let parentResult = document.getElementById('result');
-let buttons = document.querySelectorAll("buttons")
-var result = document.createElement('result');
-gameArray.forEach(button=>{
-  button.addEventListener('click', playRound())
-})
-
-let computerPlay =( )=>{   
-  return gameArray[Math.floor(Math.random()*gameArray.length)];   
-  }
-function playRound (gArray, computer) {
-  let w = "win";
-  let l = "lose"
-  for (let index = 0; index < gArray.length; index++) {
-    const button = gArray[index];
-    if (button === computer){return "Draw"}  
-    if (button === s && computer === r){
-    var newDiv = document.createElement('p');
-    newDiv.textContent = l;
-    result.appendChild(newDiv)} 
-    if (button === p && computer === s)
-    {    var newDiv = document.createElement('p');
-    newDiv.textContent = l;
-    result.appendChild(newDiv)}
-    if (button === r && computer === p)
-    {    var newDiv = document.createElement('p');
-    newDiv.textContent = l;
-    result.appendChild(newDiv)}
-    if (button === s && computer === p)
-    {    var newDiv = document.createElement('p');
-    newDiv.textContent = w;
-    result.appendChild(newDiv)}
-    if (button === r && computer === s)
-    {    var newDiv = document.createElement('p');
-    newDiv.textContent = w;
-    result.appendChild(newDiv)} 
-    if (button === p && computer === r)
-    {    var newDiv = document.createElement('p');
-    newDiv.textContent = w;
-    result.appendChild(newDiv)}  
-  }
-       
+const rpsGameArray = ["Rock", "Paper", "Scissors"];
+function computerPlay() {
+return rpsGameArray[~~(Math.random() * 3)].toLowerCase();
 }
-
-
-
+let buttons = document.querySelectorAll('.buttons')
+let result = document.getElementById('result');
+  function playRound (button, computer) {
+    let newDiv = document.createElement('p')
+    if (button === computer)
+    {    newDiv.textContent = "Its a draw";
+    newDiv.classList.add('draw');
+    result.appendChild(newDiv)}  
+    else if (button === "scissor" && computer === "rock" || button === "paper" && computer === "scissor" || button === "rock" && computer === "paper")
+    {
+    newDiv.textContent = `You entered ${button}, and the computer entered ${computer} you lost`;
+    result.appendChild(newDiv)} 
+    else  {
+    newDiv.textContent = `You entered ${button}, and the computer entered ${computer} you won!`;
+    result.appendChild(newDiv)} 
+  }
 buttons.forEach(btn=>{
-  btn.addEventListener('click', playRound)
-})
+  btn.addEventListener('click', ()=>{
+    let img = btn.querySelector('img')
+    let playerSelection = img.id;
+  let computerResult = computerPlay();
+  playRound(playerSelection, computerResult)
+  }, false)})
+
+
 
  
 
 
- let playfunction = playRound(gameArray, computerPlay());
-
-
-// let win = ()=>{ result.textContent = 'You won'}
-// let loss = ()=>{ result.textContent = 'You Lost'}
-
-
-
-let howManyRounds = ()=>{
- document.write('How Many Rounds?')
-}
-
-// var newDiv = document.createElement('----text----');
-// newDiv.classList.add('----text----');
-// newDiv.classList.remove('----text----');
-// newDiv.classList.contains('----text----');
-// result.appendChild(newDiv)
 
 
 
@@ -84,15 +43,7 @@ let howManyRounds = ()=>{
 
 
 
-// //  let  computerSelection = computerPlay()
 
 
 
 
-// // this is the repeater of our main function
-// function game (){
-//         for (let index = 0; index < 5; index++) {
-//         playRound(buttons, computerPlay);
-//         }
-//     }
-// game();
